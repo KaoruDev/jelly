f39fada9cfc3d6539229b207386b5ded08b6360e
Kaoru Kohashigawa <kaoru@spredfast.com>
2015-03-01 13:49:02 -0800

    Organize deploys to aws a bit better

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -2,13 +2,24 @@ var gulp = require('gulp');
 var aws = require('gulp-awspublish');
 var configs = require('../app-configs.js').aws;
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
   var publisher = aws.create(configs.bucket);
+  var folders = [
+    {blob: './build/scripts/*.js', dir: '/scripts'},
+    {blob: './build/styles/*.css', dir: '/styles'},
+    {blob: './build/fonts/*', dir: '/fonts'}
+  ]
 
-  return gulp.src(['./build/scripts/*.js', './build/styles/*.css'])
-    .pipe(aws.gzip())
-    .pipe(publisher.publish({}))
-    .pipe(publisher.cache())
-    .pipe(aws.reporter());
+  _.each(folders, function (configs) {
+     gulp.src(configs.blob)
+      .pipe(rename(function (path) {
+        path.dirname += configs.dir
+      }))
+      .pipe(aws.gzip())
+      .pipe(publisher.publish({}))
+      .pipe(publisher.cache())
+      .pipe(aws.reporter());
+  });
+
 });
 
