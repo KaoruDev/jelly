93a3114ad4ec5791ebb7c4dc5e9c4d65adbcdd68
Kaoru Kohashigawa <kaoru@spredfast.com>
2015-03-02 10:09:21 -0800

    Install vendor-css deps and only run if there are files to move

--- gulp/tasks/vendor-css.js
+++ gulp/tasks/vendor-css.js
@@ -3,8 +3,10 @@ var concat = require('gulp-concat-css');
 var config = require('../app-configs.js').vendorCss;
 
 gulp.task('vendor-css', function () {
-  return gulp.src(config)
-    .pipe(concat('vendor.scss'))
-    .pipe(gulp.dest('tmp/'));
+  if (config[0]) {
+    return gulp.src(config)
+      .pipe(concat('vendor.scss'))
+      .pipe(gulp.dest('tmp/'));
+  }
 });
 
