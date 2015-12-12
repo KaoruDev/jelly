21625ebbb517523cbfc1e45037f029e00d7de14b
Kaoru Kohashigawa <dev@kaoruk.com>
2015-12-12 14:18:27 -0800

    decide not to allow gulpfile to be installed in different directory of jelly

--- install.js
+++ install.js
@@ -3,17 +3,14 @@
 var configs = require('./gulp/configs');
 var exec = require('child_process').execSync;
 var processOptions = { stdio: 'inherit' };
-var targetRoot = process.argv[2] || '.';
 
 configs.announce('Installing Jelly!');
 
 configs.announce('Installing Dependencies');
 exec('npm install --save-dev' + configs.dependencies.join(' '), processOptions);
 
-require('harmony');
-
 console.log('Copying over gulpfile.js');
-exec(`cp ./jelly/gulp/configs/gulpfile-template.js ${targetRoot}/gulpfile.js`, processOptions);
+exec('cp ./jelly/gulp/configs/gulpfile-template.js ./gulpfile.js', processOptions);
 
 console.log('Creating tasks/configs/path-builder.js');
 exec('mkdir -p ./tasks/configs/', processOptions);
