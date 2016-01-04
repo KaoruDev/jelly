a1c67ee17ebc9dc9816830f780f569e3403c1940
Kaoru Kohashigawa <dev@kaoruk.com>
2016-01-04 07:08:53 -0800

    have install also create scripts styles and htmls directory along with starter files

--- install.js
+++ install.js
@@ -20,5 +20,17 @@ console.log('Creating webpack configs');
 exec('mkdir -p ./tasks/configs/webpack', processOptions);
 exec('cp ./jelly/gulp/configs/webpack-configs-template.js ./tasks/configs/webpack/configs.js', processOptions);
 
+console.log('Creating scripts...');
+exec('mkdir -p ./scripts', processOptions);
+exec('touch ./scripts/index.js', processOptions);
+
+console.log('Creating styles...');
+exec('mkdir -p ./styles', processOptions);
+exec('touch ./styles/main.scss', processOptions);
+
+console.log('Creating htmls...');
+exec('mkdir -p ./htmls', processOptions);
+exec('touch ./htmls/index.html', processOptions);
+
 configs.announce('May the Jelly be with you!');
 
