78219dc971436c9e9c8c2cfb9f0b7b8509810f2b
Kaoru Kohashigawa <dev@kaoruk.com>
2016-01-04 06:09:45 -0800

    copy htmls over into dist

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,8 +3,10 @@ var utils = require('../utils');
 var paths = require('../configs/shim/path-builder.js');
 
 gulp.task('html', function (done) {
-  utils.doesDirExist(`${paths.htmls}`).then( function () {
-    done();
+  utils.doesDirExist(`${paths.htmls}`).then(function () {
+    gulp.src(`${paths.htmls}**/*.html`)
+     .pipe(gulp.dest(paths.dist))
+     .on('finish', done);
   }).catch(function (err) {
     utils.warn(`Could not find htmls directory: ${err}`);
     done();
