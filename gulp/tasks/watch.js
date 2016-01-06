a75cb3dd2063b64a8332a77a4b6b95f74ad62912
Kaoru Kohashigawa <dev@kaoruk.com>
2016-01-06 11:54:31 -0800

    separate dev task to make it easier to overwrite

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var paths = require('../configs/shim/path-builder.js');
+var devSass = require('./sass.js');
+var htmls = require('./htmls.js');
+var watch = require('gulp-watch');
+
+gulp.task('watch', function () {
+  browserSync.init({
+    open: false,
+    server: paths.dist,
+  });
+
+  watch(`${paths.styles}**/\*.scss`, function () {
+    devSass().pipe(browserSync.stream());
+  });
+
+  watch(`${paths.htmls}**/\*.html`, function () {
+    htmls().pipe(browserSync.stream());
+  });
+
+  watch(`${paths.dist}scripts/\**/\*.js`, function () {
+    browserSync.reload('*.js');
+  });
+});
+
